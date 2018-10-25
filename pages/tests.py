a1358902a7f690678dbb9ec8e19fb73b358298f4
Alex <harrison.alex117@gmail.com>
2018-10-25 09:24:19 -0700

    wrote a test script

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+#using SimpleTestCase instead of, TestCase, because not using a database
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class PagesTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        #200 is the standard response for a successful HTTP request
+        #checking to see if status code for each page = 200
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
